4e6103f359d8c45ddacf67c9354adc421d9de69e
yeosu <ceh2805@gmail.com>
2017-05-19 17:39:09 +0900

    Update AnswerDAO.java

--- src/com/catchmind/pro/dao/AnswerDAO.java
+++ src/com/catchmind/pro/dao/AnswerDAO.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
+//
 import com.catchmind.pro.util.DataBaseConnection;
 
 public class AnswerDAO {
